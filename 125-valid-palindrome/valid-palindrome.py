aec87e37facdf021df79d1a74a191525723976e4
Adhithiya V <adhithiyav358@gmail.com>
2026-09-19 22:25:59 +0530

    Time: 123 ms (30.82%) | Memory: 13.1 MB (37.79%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def isPalindrome(self, s):
-        a = ""
-        for i in s:
+       a = ""
+       for i in s:
+            # .isalnum() checks for both letters and numbers
             if i.isalnum():
-                a+=i.lower()
-        right = len(a)-1        
-        for j in range(0,len(a)//2):
-            if a[j]!= a[right]:
-                return False
-            right -= 1
-        return True        
+                a += i
+        
+        # Reassign the lowercase version back to a
+       a = a.lower()
+        
+        # Check if it reads the same forward and backward
+       return a == a[::-1]    
